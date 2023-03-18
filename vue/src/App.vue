2df9bf806e12246cb92f6a9b9983af6ec6b54ccd
NbMiranda <nathanbmiranda@hotmail.com>
2023-03-17 21:03:39 -0300

    Library IV Laravel/Vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <LoginComponent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginComponent from './components/LoginComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import '~bootstrap/dist/css/bootstrap.css';
 </style>
